efbbc0dabcd9caa9491e7cfc8285c149c1b1d069
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-06-08 18:50:39 +1200

    Add code for first api test

--- tests/api/auth/login.api.spec.ts
+++ tests/api/auth/login.api.spec.ts
@@ -1,5 +1,12 @@
 import { test, expect } from '@playwright/test';
 
 test('POST /users/login - valid credentials returns token', async ({ request}) => {
+    const response = await request.post('/users/login', {
+        data: {
+            email: 'customer@practicesoftwaretesting.com',
+            password: 'welcome01',
+        },
+    });
 
+    expect(response.status()).toBe(200);
 });
