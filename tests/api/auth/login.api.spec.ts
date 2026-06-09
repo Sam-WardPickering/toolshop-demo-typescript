ad429351e4cdb10487c29c7875c24cdebf4d82dd
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-06-09 17:46:58 +1200

    Add interface for login response

--- tests/api/auth/login.api.spec.ts
+++ tests/api/auth/login.api.spec.ts
@@ -1,5 +1,10 @@
 import { test, expect } from '@playwright/test';
 
+interface LoginResponse {
+    access_token: string;
+    token_type: string;
+};
+
 test('POST /users/login - valid credentials returns token', async ({ request}) => {
     const response = await request.post('/users/login', {
         data: {
@@ -10,7 +15,7 @@ test('POST /users/login - valid credentials returns token', async ({ request}) =
 
     expect(response.status()).toBe(200);
 
-    const responseBody = await response.json();
+    const responseBody: LoginResponse = await response.json();
 
     expect(responseBody.access_token).toBeDefined();
     expect(responseBody.token_type).toBe('bearer');
