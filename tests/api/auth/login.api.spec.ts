c2553023df99a9ba026f8705eba7b74c42cd08cd
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-06-10 20:21:40 +1200

    Assert error status and return message

--- tests/api/auth/login.api.spec.ts
+++ tests/api/auth/login.api.spec.ts
@@ -73,14 +73,14 @@ test('POST /users/login - no email or password values', async ({ request}) => {
 
 test('POST /users/login - invalid email format', async ({ request}) => {
     const response = await request.post('/users/login', {
-    data: {
-        email: 'notanemail!',
-        password: 'welcome01'
-    },
+        data: {
+            email: 'notanemail!',
+            password: 'welcome01'
+        },
     });
 
-    // expect(response.status()).toBe(401);
+    expect(response.status()).toBe(401);
 
-    // const responseJson: LoginErrorResponse = await response.json();
-    // expect(responseJson.error).toBe('Invalid login request');
+    const responseJson: LoginErrorResponse = await response.json();
+    expect(responseJson.error).toBe('Unauthorized');
 });
