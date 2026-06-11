eb513be056a22967b87e9f2110a04e4e56ca01ee
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-06-11 22:10:17 +1200

    Add interfaces

--- tests/api/auth/favourites.api.spec.ts
+++ tests/api/auth/favourites.api.spec.ts
@@ -6,7 +6,23 @@ interface LoginResponse {
 };
 
 interface Product {
-   
+    id: string
+    name: string
+    description: string
+    price: number
+    is_location_offer: boolean
+    is_rental: boolean
+    co2_rating: string
+    in_stock: boolean
+    is_eco_friendly: boolean
+    product_image: Record<string, unknown>
+};
+
+interface FavoriteItem {
+    id: string
+    user_id: string
+    product_id: string
+    product: Product
 }
 
 test('GET /favorites - retrieves all favourites', async ({ request }) => {
