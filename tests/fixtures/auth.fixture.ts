8cf1c8533c5ccbb9708e7ba769d7a9fb3fe9f632
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-06-14 16:57:13 +1200

    Add fixture

--- tests/fixtures/auth.fixture.ts
+++ tests/fixtures/auth.fixture.ts
@@ -3,3 +3,19 @@ import { test as base } from '@playwright/test';
 interface AuthFixtures {
     token: string;
 }
+
+export const test = base.extend<AuthFixtures>({
+  token: async ({ request }, use) => {
+    const response = await request.post('/users/login', {
+      data: {
+        email: 'customer@practicesoftwaretesting.com',
+        password: 'welcome01',
+      },
+    });
+
+    const body = await response.json();
+    await use(body.access_token);
+  },
+});
+
+export { expect } from '@playwright/test';
