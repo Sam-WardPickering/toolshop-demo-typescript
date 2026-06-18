98cd04767d9d668e960bc5262d479420c17ebb4d
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-06-18 21:29:35 +1200

    Refactor test structure for sql-injection

--- tests/api/security/sql-injection.api.spec.ts
+++ tests/api/security/sql-injection.api.spec.ts
@@ -7,18 +7,24 @@ const sqlInjectionPayloads = [
     "' UNION SELECT * FROM users--",
 ];
 
-for (const payload of sqlInjectionPayloads) {
-    test(`POST /users/login - SQL injection payload: ${payload}`, async ({ request }) => {
-        const response = await request.post('/users/login', {
-            data: {
-                email: payload,
-                password: 'welcome01',
-            },
+test.describe('Login Email field - SQL injection', () => {
+    for (const payload of sqlInjectionPayloads) {
+        test(`POST /users/login - SQL injection payload: ${payload}`, async ({ request }) => {
+            const response = await request.post('/users/login', {
+                data: {
+                    email: payload,
+                    password: 'welcome01',
+                },
+            });
+
+            expect(response.status()).toBe(401);
+
+            const responseBody = await response.json();
+            expect(responseBody.error).toBe('Unauthorized');
         });
+    };
+});
 
-        expect(response.status()).toBe(401);
 
-        const responseBody = await response.json();
-        expect(responseBody.error).toBe('Unauthorized');
-    });
-};
+
+
