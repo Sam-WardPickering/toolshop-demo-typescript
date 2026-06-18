c5c0d52aaf6bd8d65aa13eb95a8afd8f352444ab
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-06-18 12:46:55 +1200

    Create sql injection payloads array

--- tests/api/security/sql-injection.api.spec.ts
+++ tests/api/security/sql-injection.api.spec.ts
@@ -1,5 +1,12 @@
 import { test, expect } from '@playwright/test';
 
+const sqlInjectionPayloads = [
+    "' OR '1'='1",
+    "'; DROP TABLE users; --",
+    "admin'--",
+    "' UNION SELECT * FROM users--",
+];
+
 test('POST /users/login - SQL injection in email field', async ({ request }) => {
     const response = await request.post('/users/login', {
         data: {
