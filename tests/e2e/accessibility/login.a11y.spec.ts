fd6048e5d267db36ed265fcc74ff5d6f163703fc
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-06-19 17:47:20 +1200

    Refactor accessibility test to check for critical or serious violations

--- tests/e2e/accessibility/login.a11y.spec.ts
+++ tests/e2e/accessibility/login.a11y.spec.ts
@@ -1,10 +1,14 @@
 import { test, expect } from '@playwright/test';
 import AxeBuilder from '@axe-core/playwright';
 
-test('Login page has no accessibility violations', async ({ page }) => {
+test('Login page has no critical or serious accessibility violations', async ({ page }) => {
   await page.goto('/auth/login');
 
   const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
 
-//   expect(accessibilityScanResults.violations).toEqual([]);
+  const seriousViolations = accessibilityScanResults.violations.filter(
+    (violation) => violation.impact === 'critical' || violation.impact === 'serious'
+  );
+
+  expect(seriousViolations).toEqual([]);
 });
