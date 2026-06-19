b788089c8a228c6bfb7b0f1cb9dc7b11c98e3419
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-06-19 16:56:31 +1200

    Update config to use multiple projects with different baseURLS

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,27 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
-  use: {
-    baseURL: 'https://api.practicesoftwaretesting.com',
-  },
+
+  projects: [
+    {
+      name: 'api',
+      testDir: './tests/api',
+      use: {
+        baseURL: 'https://api.practicesoftwaretesting.com',
+      },
+    },
+    {
+      name: 'e2e',
+      testDir: './tests/e2e',
+      use: {
+        baseURL: 'https://practicesoftwaretesting.com',
+        ...devices['Desktop Chrome'],
+      },
+    },
+  ],
 });
