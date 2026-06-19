a14d3d55b1dca63ddd4c82b036c828b1f6b5971a
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-06-19 17:24:24 +1200

    Replace import withbarrel file

--- tests/e2e/login.spec.ts
+++ tests/e2e/login.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-import { LoginPage } from './pages/LoginPage';
+import { LoginPage } from './pages';
 import { users } from '../test-data/users';
 
 test('Existing user successfully logs in (happy path)', async ({ page }) => {
