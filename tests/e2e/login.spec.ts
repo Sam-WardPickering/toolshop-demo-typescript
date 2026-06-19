71abdc92e909a5802a4719febdf2796b97d4ff3c
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-06-19 17:23:26 +1200

    Add assertions to test && add barrel file to handle future pages

--- tests/e2e/login.spec.ts
+++ tests/e2e/login.spec.ts
@@ -1,10 +1,13 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from './pages/LoginPage';
+import { users } from '../test-data/users';
 
 test('Existing user successfully logs in (happy path)', async ({ page }) => {
     await page.goto('/auth/login');
 
     const loginPage = new LoginPage(page);
 
-    // await loginPage.loginUser('admin@practicesoftwaretesting.com', )
+    await loginPage.loginUser(users.admin.email, users.admin.password);
+
+    await expect(page).toHaveURL(/dashboard/);
 });
