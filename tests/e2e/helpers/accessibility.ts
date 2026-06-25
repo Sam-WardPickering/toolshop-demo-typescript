860303214ea99dda1dea4bc7ecd4325cbf1e9171
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-06-25 17:08:49 +1200

    Add helper for serious accessiobility violations

--- tests/e2e/helpers/accessibility.ts
+++ tests/e2e/helpers/accessibility.ts
@@ -1,5 +1,7 @@
 import { AxeResults } from 'axe-core';
 
 export function getSeriousViolations(results: AxeResults) {
-    
-}
+    return results.violations.filter(
+        (violation) => violation.impact === 'critical' || violation.impact === 'serious'
+    );
+};
