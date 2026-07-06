457a52de37dcb49556151377d652e40ca021a027
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-07-06 19:57:37 +1200

    Refactor public accessibility tests structure

--- tests/e2e/accessibility/public.a11y.spec.ts
+++ tests/e2e/accessibility/public.a11y.spec.ts
@@ -2,37 +2,36 @@ import { test, expect } from '@playwright/test';
 import AxeBuilder from '@axe-core/playwright';
 import { getSeriousViolations, logNonBlockingViolations } from '../helpers/accessibility';
 
-test('Login page has no critical or serious accessibility violations', async ({ page }) => {
-  await page.goto('/auth/login');
+test.describe('Public page accessibility', () => {
+    test('Login page has no critical or serious accessibility violations', async ({ page }) => {
+        await page.goto('/auth/login');
 
-  const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+        const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
 
-  logNonBlockingViolations(accessibilityScanResults);
+        logNonBlockingViolations(accessibilityScanResults);
 
-  expect(getSeriousViolations(accessibilityScanResults)).toEqual([]);
-  
-});
-
-
-test('Product page has no critical or serious accessibility violations', async ({ page }) => {
-  await page.goto('/');
-
-  const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
-
-  logNonBlockingViolations(accessibilityScanResults);
-
-  expect(getSeriousViolations(accessibilityScanResults)).toEqual([]);
-  
-});
+        expect(getSeriousViolations(accessibilityScanResults)).toEqual([]);
+    });
 
 
-test('Contact page has no critical or serious accessibility violations', async ({ page }) => {
-  await page.goto('/');
+    test('Product page has no critical or serious accessibility violations', async ({ page }) => {
+        await page.goto('/');
 
-  const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+        const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
 
-  logNonBlockingViolations(accessibilityScanResults);
+        logNonBlockingViolations(accessibilityScanResults);
 
-  expect(getSeriousViolations(accessibilityScanResults)).toEqual([]);
-  
+        expect(getSeriousViolations(accessibilityScanResults)).toEqual([]);
+    });
+
+
+    test('Contact page has no critical or serious accessibility violations', async ({ page }) => {
+        await page.goto('/');
+
+        const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+        logNonBlockingViolations(accessibilityScanResults);
+
+        expect(getSeriousViolations(accessibilityScanResults)).toEqual([]);
+    });
 });
