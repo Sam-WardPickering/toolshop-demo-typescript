652137b4c95dddc00e79742a02dc1d51ea0d19dd
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-07-06 12:49:53 +1200

    Consolidate accessibility tests by group

---
+++ tests/e2e/accessibility/public.a11y.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect } from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright';
+import { getSeriousViolations, logNonBlockingViolations } from '../helpers/accessibility';
+
+test('Login page has no critical or serious accessibility violations', async ({ page }) => {
+  await page.goto('/auth/login');
+
+  const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+  logNonBlockingViolations(accessibilityScanResults);
+
+  expect(getSeriousViolations(accessibilityScanResults)).toEqual([]);
+  
+});
+
+
+test('Product page has no critical or serious accessibility violations', async ({ page }) => {
+  await page.goto('/');
+
+  const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+  logNonBlockingViolations(accessibilityScanResults);
+
+  expect(getSeriousViolations(accessibilityScanResults)).toEqual([]);
+  
+});
+
+
+test('Contact page has no critical or serious accessibility violations', async ({ page }) => {
+  await page.goto('/');
+
+  const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+  logNonBlockingViolations(accessibilityScanResults);
+
+  expect(getSeriousViolations(accessibilityScanResults)).toEqual([]);
+  
+});
