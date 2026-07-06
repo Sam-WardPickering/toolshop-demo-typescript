343485575b70904472ddeb871c7be65d8beba432
Sam-WardPickering <samuel.wardpickering@gmail.com>
2026-07-06 16:58:36 +1200

    Add accessibility tests for admin pages

--- tests/e2e/accessibility/admin.a11y.spec.ts
+++ tests/e2e/accessibility/admin.a11y.spec.ts
@@ -21,3 +21,122 @@ test('Admin Dashboard page has no critical or serious accessibility violations',
     expect(getSeriousViolations(accessibilityScanResults)).toEqual([]);
   
 });
+
+
+test('Admin Brands page has no critical or serious accessibility violations', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await page.goto('/auth/login');
+
+    await loginPage.loginUser(admin.email, admin.password);
+
+    await page.goto('/admin/brands');
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+    logNonBlockingViolations(accessibilityScanResults);
+
+    expect(getSeriousViolations(accessibilityScanResults)).toEqual([]);
+  
+});
+
+
+test('Admin Categories page has no critical or serious accessibility violations', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await page.goto('/auth/login');
+
+    await loginPage.loginUser(admin.email, admin.password);
+
+    await page.goto('/admin/categories');
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+    logNonBlockingViolations(accessibilityScanResults);
+
+    expect(getSeriousViolations(accessibilityScanResults)).toEqual([]);
+  
+});
+
+
+test('Admin Products page has no critical or serious accessibility violations', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await page.goto('/auth/login');
+
+    await loginPage.loginUser(admin.email, admin.password);
+
+    await page.goto('/admin/products');
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+    logNonBlockingViolations(accessibilityScanResults);
+
+    expect(getSeriousViolations(accessibilityScanResults)).toEqual([]);
+  
+});
+
+
+test('Admin Orders page has no critical or serious accessibility violations', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await page.goto('/auth/login');
+
+    await loginPage.loginUser(admin.email, admin.password);
+
+    await page.goto('/admin/orders');
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+    logNonBlockingViolations(accessibilityScanResults);
+
+    expect(getSeriousViolations(accessibilityScanResults)).toEqual([]);
+  
+});
+
+
+test('Admin Users page has no critical or serious accessibility violations', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await page.goto('/auth/login');
+
+    await loginPage.loginUser(admin.email, admin.password);
+
+    await page.goto('/admin/users');
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+    logNonBlockingViolations(accessibilityScanResults);
+
+    expect(getSeriousViolations(accessibilityScanResults)).toEqual([]);
+  
+});
+
+
+test('Admin Messages page has no critical or serious accessibility violations', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await page.goto('/auth/login');
+
+    await loginPage.loginUser(admin.email, admin.password);
+
+    await page.goto('/admin/messages');
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+    logNonBlockingViolations(accessibilityScanResults);
+
+    expect(getSeriousViolations(accessibilityScanResults)).toEqual([]);
+  
+});
+
+
+test('Admin Settings page has no critical or serious accessibility violations', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await page.goto('/auth/login');
+
+    await loginPage.loginUser(admin.email, admin.password);
+
+    await page.goto('/admin/settings');
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+    logNonBlockingViolations(accessibilityScanResults);
+
+    expect(getSeriousViolations(accessibilityScanResults)).toEqual([]);
+  
+});
